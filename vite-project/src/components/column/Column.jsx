2737f2780570512b4b59c3dfc001f46c459c65ba
KirlYurch <pupyrkin.1970@mail.ru>
2024-03-15 20:02:51 +0500

    hm-1 почти

---
+++ vite-project/src/components/column/Column.jsx
@@ -0,0 +1,43 @@
+/* eslint-disable react/jsx-key */
+import Card from "../card/Card";
+
+const tasks = [
+  {
+    title: "Название задачи ",
+    date: "30.10.2023",
+    theme: "green",
+    description: "Research",
+  },
+  {
+    title: "Название задачи ",
+    date: "31.10.2023",
+    theme: "orange",
+    description: "Web-designer",
+  },
+  {
+    title: "Название задачи ",
+    date: "31.10.2023",
+    theme: "purple",
+    description: "Web-designer",
+  },
+];
+export default function Column({ title }) {
+  return (
+    <div className="main__column column">
+      <div className="column__title">
+        <p>{title}</p>
+      </div>
+
+      <div className="cards">
+        {tasks.map((task) => (
+          <Card
+            title={task.title}
+            date={task.date}
+            theme={task.theme}
+            description={task.description}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ vite-project/src/components/main/Main.jsx
@@ -0,0 +1,23 @@
+import Column from "../column/Column";
+export default function Main() {
+return (
+    <main className="main">
+    <div className="container">
+      <div className="main__block">
+        <div className="main__content">
+          {/* Тут начало  общего блока! */}
+
+          {/* Окончание блоково компонент */}
+          <Column title={`Без статуса`} />
+          <Column title={`Нужно сделать`} />
+          <Column title={`В работе`} />
+          <Column title={`Тестирование`} />
+          <Column title={`Готово`} />
+          {/* ................................................................. */}
+        </div>
+      </div>
+    </div>
+  </main>
+  );
+}
+
